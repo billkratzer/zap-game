b29232cee61b5342e1c53b22aacfa32ccfb82f6d
Bill Kratzer <bill@Bills-Home-MBP.home>
2021-09-06 08:21:16 -0400

    using kanit font.  code clean up

--- src/js/game/scene/TitleScene.js
+++ src/js/game/scene/TitleScene.js
@@ -24,40 +24,22 @@ class TitleScene extends Phaser.Scene {
             delay: 1000
         });
 
-        let authorText = this.add.bitmapText(0, 0, 'helvetica-neue-thin-gray-24', 'By Bill Kratzer & Tayler Ramsay', 24)
-        authorText.setOrigin(0.5, 0.5)
-        authorText.setPosition(camera.width + authorText.width, camera.centerY * 1.1)
-
-        this.tweens.add({
-            targets: authorText,
-            x: camera.centerX,
-            duration: 1000,
-            ease: 'Back',
-            easeParams: [ 3.5 ],
-            delay: 1000
-        });
-
-        let pressText = this.add.bitmapText(0, 0, 'helvetica-neue-white-32', 'Press Any Key to Start!', 32)
+        let pressText = this.add.bitmapText(0, 0, 'kanit-32-thin', 'Press Any Key to Start!', 32)
         pressText.setOrigin(0.5, 0.5)
         pressText.setPosition(camera.centerX, camera.height + pressText.height)
 
         this.tweens.add({
             targets: pressText,
-            y: camera.centerY * 1.4,
+            y: camera.centerY * 1.2,
             duration: 1000,
             ease: 'Back',
-            easeParams: [ 3.5 ],
+            easeParams: [ 2 ],
             delay: 1000
         });
 
         let padding = 5;
-        let copyrightText = this.add.text(padding, camera.height - padding, 'Copyright (c) 2021 Loud Shirt Studios')
-            .setFontFamily('Helvetica')
-            .setFontSize(12)
-            .setColor('#aaa')
-            .setOrigin(0, 1)
 
-        let versionText = this.add.text(camera.width - padding, camera.height - padding, 'Version: ' + globals.version)
+        this.add.text(camera.width - padding, camera.height - padding, 'Version: ' + globals.version)
             .setFontFamily('Helvetica')
             .setFontSize(12)
             .setColor('#aaa')
@@ -81,7 +63,6 @@ class TitleScene extends Phaser.Scene {
     }
 
     update() {
-
     }
 
 }
